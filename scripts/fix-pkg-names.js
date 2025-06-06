6032f11df4e9fa2d814c67037d6adda2f7f0dc19
menvae <menvaedev@gmail.com>
2025-06-06 17:20:03 +0300

    npm publishing

---
+++ scripts/fix-pkg-names.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const nodePackagePath = path.join(__dirname, '../dist-node/package.json');
+const nodePackage = JSON.parse(fs.readFileSync(nodePackagePath, 'utf8'));
+nodePackage.name = 'rgc-chart-node';
+fs.writeFileSync(nodePackagePath, JSON.stringify(nodePackage, null, 2));
+console.log('Node package name updated');
+
+const webPackagePath = path.join(__dirname, '../dist-web/package.json');
+const webPackage = JSON.parse(fs.readFileSync(webPackagePath, 'utf8'));
+webPackage.name = 'rgc-chart-web';
+fs.writeFileSync(webPackagePath, JSON.stringify(webPackage, null, 2));
+console.log('Web package name updated');
+
+console.log('All package names updated');
