3cf7b714e5fb0bc10c807ecf160d98935fe8cfa4
menvae <menvaedev@gmail.com>
2025-06-18 12:24:24 +0300

    added keywords for crate.io package I can't believe I added keywords for npm but forgot for cargo

--- scripts/fix-pkg.js
+++ scripts/fix-pkg.js
@@ -1,28 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
-const Keywords = [
-  "wasm",
-  "rust",
-  "rhythm-game",
-  "osu",
-  "stepmania",
-  "parser",
-  "converter",
-  "writer"
-];
-
 const nodePackagePath = path.join(__dirname, '../dist-node/package.json');
 const nodePackage = JSON.parse(fs.readFileSync(nodePackagePath, 'utf8'));
 nodePackage.name = 'rgc-chart-nodejs';
-nodePackage.keywords = Keywords;
 fs.writeFileSync(nodePackagePath, JSON.stringify(nodePackage, null, 2));
 console.log('Node package name and keywords updated');
 
 const webPackagePath = path.join(__dirname, '../dist-web/package.json');
 const webPackage = JSON.parse(fs.readFileSync(webPackagePath, 'utf8'));
 webPackage.name = 'rgc-chart-browser';
-webPackage.keywords = Keywords;
 fs.writeFileSync(webPackagePath, JSON.stringify(webPackage, null, 2));
 console.log('Web package name and keywords updated');
 
