c8553592939928f5aa8f15e8a2efc8c48a6f4d5b
menvae <menvaedev@gmail.com>
2025-06-18 12:25:23 +0300

    small correction

--- scripts/fix-pkg.js
+++ scripts/fix-pkg.js
@@ -5,12 +5,12 @@ const nodePackagePath = path.join(__dirname, '../dist-node/package.json');
 const nodePackage = JSON.parse(fs.readFileSync(nodePackagePath, 'utf8'));
 nodePackage.name = 'rgc-chart-nodejs';
 fs.writeFileSync(nodePackagePath, JSON.stringify(nodePackage, null, 2));
-console.log('Node package name and keywords updated');
+console.log('Node package name updated');
 
 const webPackagePath = path.join(__dirname, '../dist-web/package.json');
 const webPackage = JSON.parse(fs.readFileSync(webPackagePath, 'utf8'));
 webPackage.name = 'rgc-chart-browser';
 fs.writeFileSync(webPackagePath, JSON.stringify(webPackage, null, 2));
-console.log('Web package name and keywords updated');
+console.log('Web package name updated');
 
-console.log('All package names and keywords updated');
+console.log('All package names updated');
